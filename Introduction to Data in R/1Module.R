a64b9c6a642ea491866bc73771ab28b458b09a85
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2020-09-27 00:36:53 +0530

    learning of 26Sep2020

---
+++ Introduction to Data in R/1Module.R
@@ -0,0 +1,48 @@
+setwd("E:\\Self_GitKraken\\Working_Repo_GitHub\\Data-Camp-Learn\\Introduction to Data in R")
+getwd()
+
+#####
+# Load the email50 dataset with the data() function.
+# View the structure of this dataset with the str() function. How many observations and variables are there?
+
+# Load data
+data(email50)
+
+# View the structure of the data
+str(email50)
+
+#####
+
+#Use the glimpse() function to view the variables in the email50 dataset.
+# Glimpse email50
+glimpse(email50)
+
+#
+
+
+#####
+
+
+
+
+
+#####
+
+
+
+
+
+#####
+
+
+
+
+
+
+#####
+
+
+
+
+
+#####
