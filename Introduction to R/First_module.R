ce20e9393a5a937da39621c2806996a7928c8c35
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2020-10-14 10:17:58 +0530

    Revision of R 14Oct2020

---
+++ Introduction to R/First_module.R
@@ -0,0 +1,67 @@
+getwd()
+setwd("E:\\Self_GitKraken\\Working_Repo_GitHub\\Data-Camp-Learn\\Introduction to R")
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6+12
+
+
+#Arithmetic with R
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28%%6
+
+#Variable assignment
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+##Variable assignment (2)
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+
+# Print out the value of the variable my_apples
+my_apples
+
+###Variable assignment (3)
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+print(my_apples + my_oranges)
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+####Apples and oranges
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6 
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit
+
+
