7ae16c6037c999432eb740be5ca5aa6461990d54
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2021-01-01 11:24:41 +0530

    third module starts

---
+++ Intermediate R/3_Third_Module.R
@@ -0,0 +1,35 @@
+#   start date  : 1Jan2021
+#   author      : Narendra Singh Shekhawat
+
+###functions
+help(sd)
+?sd
+
+##########
+# Consult the documentation on the mean() function
+help(mean)
+
+# Inspect the arguments of the mean() function
+args(mean)
+
+
+##########
+#Use a function
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Calculate average number of views
+avg_li <- mean(linkedin)
+avg_fb <- mean(facebook)
+
+
+# Inspect avg_li and avg_fb
+print(avg_li)
+print(avg_fb)
+
+
+#########
+#Use a function (2)
+
+
