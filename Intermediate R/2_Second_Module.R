fb399b7d7b82990dc4e0707d947559405acaaf65
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2021-01-01 11:24:29 +0530

    second module complete

--- Intermediate R/2_Second_Module.R
+++ Intermediate R/2_Second_Module.R
@@ -71,6 +71,106 @@ while (i <= 10) {
 
 
 #######For loop
+#Loop over a vector
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for ( link in linkedin ){
+  print(link)
+} 
+
+# Loop version 2
+for (i in 1:length(linkedin)){
+  print(linkedin[i])
+}
 
 
+##########3Loop over a list
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for(ny in nyc){
+  print(ny)
+}
+# Loop version 2
+for (i in 1:length(nyc)){
+  print(nyc[[i]])
+}
+
+############Loop over a matrix [important matrix]
+# The tic-tac-toe matrix ttt has already been defined for you
+
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
+  }
+}
+
+############
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Code the for loop with conditionals
+for (li in linkedin) {
+  if ( li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  print(li)
+}
+
+########Next, you break it
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Adapt/extend the for loop
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  
+  # Add if statement with break
+  if(li > 16){
+    print("This is ridiculous,I'm outta here!")
+    break
+  }
+  
+  # Add if statement with next
+  if(li<5){
+    print("This is too embarrassing!")
+    next
+  }
+  
+  print(li)
+}
+
+############Build a for loop from scratch
+# Pre-defined variables
+rquote <- "r's internals are irrefutably intriguing"
+chars <- strsplit(rquote, split = "")[[1]]
+
+# Initialize rcount
+rcount <- 0
+
+# Finish the for loop
+for (char in chars) {
+  if(char == "r")
+    rcount <- rcount +1 
+  if (char =="u"){
+    break
+  }
+  
+}
+
+# Print out rcount
+print(rcount)
+  
 
