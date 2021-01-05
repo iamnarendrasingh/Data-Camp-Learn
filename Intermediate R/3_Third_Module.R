594e5637f79fc8dbb63f5b2d0f5a50bc06f0f84b
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2021-01-05 15:30:25 +0530

    completed 3rd module

--- Intermediate R/3_Third_Module.R
+++ Intermediate R/3_Third_Module.R
@@ -96,3 +96,55 @@ interpret <- function(num_views) {
 # Call the interpret function twice
 interpret(linkedin[1])
 interpret(facebook[2])
+
+
+
+##################
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# The interpret() can be used inside interpret_all()
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return(num_views)
+  } else {
+    print("Try to be more visible!")
+    return(0)
+  }
+}
+
+# Define the interpret_all() function
+# views: vector with data to interpret
+# return_sum: return total number of views on popular days?
+interpret_all <- function(views, return_sum = TRUE) {
+  count <- 0
+  
+  for (v in views) {
+    count <- interpret(v) + count
+  }
+  
+  if (return_sum==TRUE) {
+    print(count)
+  } else {
+    return(0)
+  }
+}
+
+# Call the interpret_all() function on both linkedin and facebook
+interpret_all(linkedin)
+interpret_all(facebook)
+
+
+
+################### R package
+
+#####Load an R Package
+# Load the ggplot2 package
+library(ggplot2)
+# Retry the qplot() function
+qplot(mtcars$wt, mtcars$hp)
+
+# Check out the currently attached packages again
+search()
